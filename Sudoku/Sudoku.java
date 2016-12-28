e77eda4a1ee990a9736080f0062100304b0cddbd
Khinshan Khan <kkhan@stuy.edu>
2016-12-28 17:30:52 -0500

    Basic Start of Sudoku, importaions as many as i could thik relevant imported

--- Sudoku/Sudoku.java
+++ Sudoku/Sudoku.java
@@ -1,1 +1,18 @@
-//new
+//importations
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.util.Random;
+
+//class name+ implementations, note i like to use graphic
+public class Sudoku implements ActionListener, MouseListener{
