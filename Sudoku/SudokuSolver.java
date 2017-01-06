56dd9fbd2a3d4b7f1a697cb40f49102837ada09c
Ben Weinstein <bweinstein@stuy.edu>
2017-01-05 21:31:02 -0500

    Backtracking alg more complete. Survives (basic) test

--- Sudoku/SudokuSolver.java
+++ Sudoku/SudokuSolver.java
@@ -1,3 +1,9 @@
+/*
+NOTES: 
+1/5: finished algorithm (waiting for randomization bit for better tests, though)
+     up next: integrate GUI, add ability to build puzzle!
+ */ 
+
 //import stuff
 import java.awt.Color;
 import java.awt.Font;
@@ -10,7 +16,7 @@ import java.awt.event.MouseListener;
 import javax.swing.*;
 
 public class SudokuSolver{
-    private String[][] solvedPuzzle = {
+    private String[][] puzzle = {
 	{"6", "1", "3", "5", "4", "2", "8", "9", "7"},
 	{"8", "9", "7", "3", "6", "1", "5", "4", "2"},
 	{"5", "4", "2", "9", "8", "7", "3", "1", "6"}, 
@@ -22,12 +28,14 @@ public class SudokuSolver{
 	{"9", "8", "4", "2", "1", "5", "6", "7", "3"}}; //decided on Strings
 
     
-    public SudokuSolver(){
-
+    public SudokuSolver(String[][] p){
+	puzzle = p;
     }
 
     //BOOLEAN BACKTRACKING FUNCTION (ESSENTIAL FOR SOLVING PUZZLES)
-    String[] vals = {"1","2","3","4","5","6","7","8","9"}; //for assigning values to the grid
+
+    //for assigning values to the grid
+    static String[] vals = {"1","2","3","4","5","6","7","8","9"};
 
     public static boolean canBeSolved(String[][] puzzle, int r, int c){
 	//static or no?
@@ -35,13 +43,24 @@ public class SudokuSolver{
 	if(r == 8 && c == 8){ //breaks loop -- base case
 	    return true;
 	}
+
+	//skip over already-filled-in boxes!!!
+	if(!(puzzle[r][c].equals(""))){
+	    if(r == 8){//if it's the end of the row, go to the next one!
+		return canBeSolved(puzzle, 0, c + 1);
+	    }
+	    else{//anywhere else in the grid
+		return canBeSolved(puzzle, r + 1, c);
+	    }
+	}
 	
+	//where you set values
 	for(int i = 1; i < vals.length + 1; i++){
-	    if(noSameRow(puzzle[r], c, vals[i]) &&
-	       noSameCol(puzzle, r, c, vals[i]) &&
-	       noSameBox(puzzle, r, c, vals[i])){
+	    if(noSameRow(puzzle[r], c, vals[i - 1]) &&
+	       noSameCol(puzzle, r, c, vals[i - 1]) &&
+	       noSameBox(puzzle, r, c, vals[i - 1])){
 		//set that puzzle grid spot to the value 
-		puzzle[r][c] = vals[i];
+		puzzle[r][c] = vals[i - 1];
 
 		//recursive call 
 		if(r == 8){//end of the row...
@@ -56,8 +75,9 @@ public class SudokuSolver{
     }
 
     //helper functions for recursive canBeSolved
+
     //checks if not any double values in same row
-    public static boolean noSameRow(String[] r, int c, String thisVal){
+    private static boolean noSameRow(String[] r, int c, String thisVal){
 	for(int i = 0; i < r.length; i++){
 	    if(i != c && r[i].equals(thisVal)){
 		return false;
@@ -67,7 +87,7 @@ public class SudokuSolver{
     }
 
     //checks if not any double values in same column 
-    public static boolean noSameCol(String[][] p, int r, int c, String thisVal){
+    private static boolean noSameCol(String[][] p, int r, int c, String thisVal){
 	for(int i = 0; i < p.length; i++){
 	    if(i != r && p[i][c].equals(thisVal)){
 		return false;
@@ -77,7 +97,7 @@ public class SudokuSolver{
     }
 
     //checks if not any double values in same box
-    public static boolean noSameBox(String[][] p, int r, int c, String thisVal){
+    private static boolean noSameBox(String[][] p, int r, int c, String thisVal){
 	//have to find the start of each box (each box 'starts' in its upper
 	//left corner).
 	int boxRowStart = r / 3 * 3; //either 0, 3, or 6
@@ -104,15 +124,35 @@ public class SudokuSolver{
 		if(j == 3 || j == 6){
 		    ans += " | ";
 		}
-		ans += puzzle[i][j] + " ";
-	    }
+		if(puzzle[i][j].equals("")){
+		    ans += "  ";
+		}
+		else{
+		    ans += puzzle[i][j] + " ";
+		}
+	    }		
 	    ans += "\n";
 	}
 	return ans;
     }
 
     public static void main(String[] args){
-	SudokuSolver a = new SudokuSolver();
-	System.out.println(a);
+	String[][] unsolvedPuzzle = {
+	{"6", "1", "3", "5", "4", "2", "8", "9", "7"},
+	{"8", "9", "", "3", "6", "1", "5", "4", "2"},
+	{"5", "4", "2", "9", "8", "7", "3", "1", "6"}, 
+	{"4", "", "1", "7", "3", "9", "2", "8", "5"},
+	{"", "5", "8", "", "2", "6", "1", "3", "9"}, 
+	{"3", "2", "9", "1", "5", "8", "7", "6", "4"},  
+	{"2", "", "6", "", "7", "4", "9", "5", ""}, 
+	{"1", "7", "5", "6", "9", "3", "4", "2", ""},
+	{"9", "8", "4", "2", "1", "5", "6", "7", "3"}};// deleting a few values for a test run of canBeSolved
+	SudokuSolver unsolved = new SudokuSolver(unsolvedPuzzle);
+       	System.out.println(unsolved);
+
+	canBeSolved(unsolvedPuzzle, 0, 0);
+	SudokuSolver solved = new SudokuSolver(unsolvedPuzzle);
+	System.out.println(solved); //should be solved now
+	//update: THAT WORKS!!!
     }
 }
