3b894355c3f548f05d5031e367b500732fe292fe
Ben Weinstein <bweinstein@stuy.edu>
2017-01-06 14:19:51 -0500

    Base for Solver GUI

---
+++ RendererSolver.java
@@ -0,0 +1,16 @@
+//THIS IS FOR THE SOLVER
+//import stuffs
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+//class name + extension
+public class RendererSolver extends JPanel{
+    private static final long serialVersionUID = 1L;
+
+    //method visible only to the class to which this belongs, and any subclasses
+    //will be used for the GUI aspect of SudokuSolver
+    protected void paintComponent(Graphics g){
+	super.paintComponent(g);
+	SudokuSolver.objectname.repaint(g);
+    }
+}
