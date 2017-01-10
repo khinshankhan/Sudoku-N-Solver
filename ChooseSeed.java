11120554ae65e75bb45081f33e88e125b91d0bd1
Khinshan Khan <kkhan@stuy.edu>
2017-01-09 23:57:37 -0500

    smart cancel

--- ChooseSeed.java
+++ ChooseSeed.java
@@ -11,6 +11,7 @@ public class ChooseSeed implements ActionListener{
     private JButton cancel;
     private JLabel seed;
     private JTextField val;
+    static int wherefrom;
 
     public ChooseSeed(){
 	JFrame jframe = new JFrame("Choose Seed");
@@ -28,6 +29,8 @@ public class ChooseSeed implements ActionListener{
 	seed = new JLabel("Seed:",null,JLabel.CENTER);
 	val = new JTextField(15);
 	cancel = new JButton("Cancel");
+	cancel.addActionListener(this);
+	cancel.setActionCommand("2");
 	ok = new JButton("OK");
 	ok.addActionListener(this);
 	ok.setActionCommand("1");
@@ -45,8 +48,28 @@ public class ChooseSeed implements ActionListener{
 	    Sudoku.main(myString);
 	   
 	}
+	if(e.getActionCommand().equals("2")){
+	    x.dispose();
+	    if(wherefrom>=0){
+	    String[] myString = {""+wherefrom};
+	    Sudoku.main(myString);
+	    }
+	    else{
+		String[] myString = {"a"};
+		Menu.main(myString);
+	    }
+	   
+	}
     }
     public static void main(String[] a){
+	int x;
+	try {
+	    x = Integer.parseInt(a[0]);
+	    x %= 1000;
+	}catch (Exception e){
+	    x = -1;
+	}
+	wherefrom = x;
 	ChooseSeed window = new ChooseSeed();
     }
 }
