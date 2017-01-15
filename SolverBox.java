9ff6d43a7c947b5e5f2ba8d0029906f44d9aec3b
Ben Weinstein <bweinstein@stuy.edu>
2017-01-15 00:21:19 -0500

    clarified message even more

--- SolverBox.java
+++ SolverBox.java
@@ -2,8 +2,10 @@ import javax.swing.*;
 
 public class SolverBox{
 
+    static String errorMessage = "Please enter a valid puzzle! There was at least one\nnumber repeated in a row, column, and/or box!";
+
     public static void main(String[] args){
-	JOptionPane.showMessageDialog(null, "Please enter a valid puzzle!", "Solver Error", JOptionPane.ERROR_MESSAGE);
+	JOptionPane.showMessageDialog(null, errorMessage, "Solver Error", JOptionPane.ERROR_MESSAGE);
     }
 }
 
